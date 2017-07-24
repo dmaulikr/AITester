baf2a19ed728e00ac20b27c3b76be6fa8e5f2270
sefdevelopment <andreisadovnicov@Andreis-MacBook-Air.local>
2017-07-24 17:57:07 +0300

    Updated the Reachability class, so that it works in Swift 3.

--- AI Tester/Reachability.swift
+++ AI Tester/Reachability.swift
@@ -9,29 +9,39 @@
 import Foundation
 import SystemConfiguration
 
-// The solution for checking whether there is an internet connection was taken from http://stackoverflow.com/questions/30743408/check-for-internet-connection-in-swift-2-ios-9.
+// The solution for checking whether there is an internet connection was taken from https://stackoverflow.com/questions/30743408/check-for-internet-connection-with-swift
 
 open class Reachability {
     
     class func isConnectedToNetwork() -> Bool {
         
-        var zeroAddress = sockaddr_in()
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
         zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
         
         let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
         }
         
-        var flags = SCNetworkReachabilityFlags()
-        
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
             return false
         }
         
+        /* Only Working for WIFI
+         let isReachable = flags == .reachable
+         let needsConnection = flags == .connectionRequired
+         
+         return isReachable && !needsConnection
+         */
+        
+        // Working for Cellular and WIFI
         let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
         let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let ret = (isReachable && !needsConnection)
         
-        return (isReachable && !needsConnection)
+        return ret
     }
 }
